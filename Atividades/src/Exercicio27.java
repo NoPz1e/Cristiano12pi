e4edd19d942a57284d7bfa52418a9870876dda8b
NoPz1e <Cristianomanuelneto2006@gmail.com>
2024-10-18 16:07:42 +0100

    alteração 27

---
+++ Atividades/src/Exercicio27.java
@@ -0,0 +1,11 @@
+public class Exercicio27 {
+    public static void main(String[] args) {
+
+        String idade = "30";
+
+        int x = Integer.parseInt(idade);
+        float y = Float.parseFloat(idade);
+
+        System.out.println("Valor int : " + x + "\nValor float: "+ y);
+    }
+}
