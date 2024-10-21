ec722cb3bf17c1483948a50d3f560209bab64e3e
NoPz1e <Cristianomanuelneto2006@gmail.com>
2024-10-21 16:26:24 +0100

    correct

--- Atividades/src/Atividade30.java
+++ Atividades/src/Atividade30.java
@@ -1,14 +1,14 @@
 public class Atividade30 {
     public static void main(String[] args) {
 
-        double[] notas = {8.5, 9.0, 7.3, 6.8, 10.0};
+        double[] notas = {17.5, 19.0, 7.3, 11.8, 10.2};
 
         double soma = 0;
         int nAlunos = 0;
 
-        for(int i = 0; i < notas.length; i++){
-            soma += notas[i];
-            nAlunos ++;
+        for (double nota : notas) {
+            soma += nota;
+            nAlunos++;
         }
 
         double media = soma / nAlunos;
